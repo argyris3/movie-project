92aa0c28d751a7780ed30a47d1ddb5818585e96f
argyris3 <78222306+argyris3@users.noreply.github.com>
2023-10-16 22:38:05 +0300

    about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-x-4 ">
+      <h1 className=" text-lg md:text-xl lg:text-2xl xl:text-3xl font-medium mb-2 text-amber-500">About</h1>
+      <p className="text-lg font-semibold md:text-xl lg:text-2xl xl:text-3xl tracking-wider  ">
+        Καλως Ηρθατε στην Ταινιοθηκη μας..Αν ειστε παθιασμενος με τις ταινιες εδω ειναι το καταλληλο μερος.Η ιστιοσελιδα
+        μας ανανεωνετε συνεχως με καινουργιες ταινιες ετσι ωστε να εχετε προσβαση στα τελευταια project στο κοσμο του
+        σινεμα.{' '}
+      </p>
+      <p className="text-lg font-semibold md:text-xl lg:text-2xl xl:text-3xl tracking-wider  ">
+        Μπορειτε να αναζητησετε ταινιες με τον τιτλο,με σκηνοθετη,ειδος,ηθοποιο η και την ημερομηνια αφιξης στις
+        αιθουσες..Επιπροσθετα μπορειτε να δειτε νεα απο τον κοσμο του σινεμα,trailers και πολλα αλλα..Ευχαριστουμε που
+        μας επισκεφτηκατε παντα κανουμε το καλυτερο που μπορουμε να βελτιωσουμε τις υπηρεσιες μας..Καλη Διασκεδαση!!{' '}
+      </p>
+    </div>
+  );
+}
+export default About;
